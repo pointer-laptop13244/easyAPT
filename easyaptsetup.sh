1bd55731d759aae3ab9fd0dde0f650bf6b72ff8f
Precious O <p14277376@gmail.com>
2026-07-25 17:43:03 +0000

    easyAPT update to JUL25-V110

--- easyaptsetup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-read -p "What is the FULL installation path:" $EASYAPTPATH
-read -p "What is the path of the .zip file, including the zip file? Type ~/thezipfile.zip for example, not ~.:" $ZIPFILEPATH
-cd /
-cd "$EASYAPTPATH"
-unzip $ZIPFILEPATH
-chmod +x *.sh
-echo "Finished"
